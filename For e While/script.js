12f67bec54981110993f2e86353a4464a8739f23
Fagner <fagnerpraxedes1@gmail.com>
2026-07-27 18:52:58 -0300

    adicionando laços de repetiçao For e While. funções Arrow function

---
+++ For e While/script.js
@@ -0,0 +1,13 @@
+
+//exemplo de for
+let numeros = [10,20,30];
+for (let i = 0; i < numeros.length; i++) {
+    console.log(numeros[i]);
+}
+
+//exemplo de for...of
+function multiplicar(a, b){
+    return a * b;
+}
+
+console.log(multiplicar(5, 4));
