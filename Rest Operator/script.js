0599dea924b988f1bf149c1b788047b38df95d88
Fagner <fagnerpraxedes1@gmail.com>
2026-08-17 15:07:27 -0300

    entendendo o Rest Operator dentro de uma função

--- Rest Operator/script.js
+++ Rest Operator/script.js
@@ -15,3 +15,21 @@ function testandoRest2(...extras){
 testandoRest2(1,2,3,4,5);
 
 // Já nesse outro exemplo, os índices são definidos dentro do array corretamente, através da função Rest.
+
+//Desafio
+//O Porteiro Educado (Default Parameters)
+// Crie uma função chamada darBoasVindas que receba o parâmetro nome. Use o Default Parameter para que, se a função for chamada sem nenhum nome, a variável nome receba o valor padrão "Visitante". A função deve imprimir no console a mensagem: "Bem-vindo(a), [nome]!"
+
+function darBoasVindas(nome = "Visitante"){
+    console.log(`Bem-vindo(a), ${nome}!`);
+}
+
+//A Lista de Compras (Rest Operator)
+// Crie uma função chamada fazerCompras que use o Rest Operator (...) nos parâmetros para receber uma quantidade indefinida de itens (por exemplo: ...itens). A função deve simplesmente imprimir no console o array contendo todos os itens que foram passados. Chame essa função passando três strings diferentes soltas (ex: "Pão", "Leite", "Café").
+
+function fazerCompras(...itens){
+    console.log(itens);
+}
+
+fazerCompras("Pão", "Leite", "Café");
+
