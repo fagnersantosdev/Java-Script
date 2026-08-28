1d1e3dcd6f33f91174e8e06130c9fd1beb332985
Fagner <fagnerpraxedes1@gmail.com>
2026-08-28 15:17:14 -0300

    adicionando Spread Operator

--- Spread Operator/script.js
+++ Spread Operator/script.js
@@ -21,3 +21,21 @@ let novaPessoa = {
 console.log(novaPessoa);
 
 // Com o Spread Operator, podemos acrescentar itens em um Array ou Objeto. Basta apenas cria-mos um novo array ou objeto, e colocarmos (...) antes do array ou objeto anterior!
+
+//Desafio
+//Juntando Arrays: Crie um array turmaA = ["Ana", "Bia"] e outro turmaB = ["Carlos", "Daniel"]. Crie um terceiro array chamado todasTurmas usando o Spread para juntar os dois em um só. Imprima no console.
+
+let turmaA = ["Ana", "Bia"];
+let turmaB = ["Carlos", "Daniel"];
+let todasTurmas = [... turmaA, "Carlos", "Daniel"];
+console.log(todasTurmas);
+
+//Atualizando Objeto: Crie um objeto produto = { nome: "Monitor", preco: 800, estoque: 10 }. Usando o Spread Operator, crie uma variável chamada produtoVendido que copie tudo de produto, mas altere o estoque para 9. Imprima no console.
+
+let produto = {
+    nome: "Monitor",
+    preco: 800,
+    estoque: 10,
+};
+
+let produtoVendido = {... produto, };
